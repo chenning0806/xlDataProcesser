84bebc5375d3887ab27d2ae95cd1467451d93fca
陈宁 <immrchenchn@gmail.com>
2017-05-24 17:37:44 +0800

    1

--- src/main/java/com/chen/util/RedisTest.java
+++ src/main/java/com/chen/util/RedisTest.java
@@ -2,11 +2,16 @@ package com.chen.util;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ChenNing on 16/5/23.
@@ -17,21 +22,31 @@ public class RedisTest {
 
     @Resource(name = "redisTemplate")
     private RedisTemplate<String, String> redisTemplate;
-
+    private static final String _userAchieve = "ach_userach:{0}:{1}";
     @Test
     public void main() throws InterruptedException {
-        BigDecimal schoolMoney = new BigDecimal("-1111");
-        System.out.println(schoolMoney.compareTo(new BigDecimal("0")));
-//        redisTemplate.delete("rede:all:rule");
-//        BoundListOperations<String,String> boundValueOperations = redisTemplate.boundListOps("rede:all:rule");
-//        List<String> l =  boundValueOperations.range(0, boundValueOperations.size());
-//        System.out.println(l);
+        redisTemplate.delete("rede:all:rule");
+        BoundListOperations<String,String> boundValueOperations = redisTemplate.boundListOps("rede:all:rule");
+        List<String> list = new ArrayList<String>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        String[] strings = new String[list.size()];
+        list.toArray(strings);
+        boundValueOperations.rightPushAll(strings);
+        List<String> l =  boundValueOperations.range(2, boundValueOperations.size());
+
+        System.out.println(l);
 
         //序列化工具
 //        List<String> s = Arrays.asList("aaa");
 //        System.out.println(s);
 //        redisTemplate.delete("rede:init:time");
 //        BoundHashOperations operations = redisTemplate.boundHashOps("rede:show");
+//        System.out.println(operations.increment("10001",-10));
+
 //        List<String> list = new ArrayList<String>();
 //        list.add("a");
 //        list.add("b");
@@ -61,8 +76,53 @@ public class RedisTest {
 //        hashOperations.delete("rude:show","10001","10002");
 //        boundValueOperations.set("rude:show","123");
 //        System.out.println(boundValueOperations.get("rude:show"));
+//        BoundHashOperations redisShowHash = redisTemplate.boundHashOps("rede:showw");
+//        String hashKey = MessageFormat.format(_userAchieve, "1000110002", null);
+//        System.out.println(hashKey);
+//        String key = 1+"_"+"0";
+//        System.out.println(getHashValueExists(hashKey, key));
+//        setHashValue(hashKey, key,"123");
+//        redisShowHash.put("","");
+
 
     }
 
 
+    /**
+     * hash结构中获取hash中field是否存在
+     *
+     * @param key
+     * @param field
+     * @return
+     */
+    public Boolean getHashValueExists(String key, String field) {
+        final byte[] keyByte = key.getBytes();
+        final byte[] fieldByte = field.getBytes();
+        Boolean b = redisTemplate.execute(new RedisCallback<Boolean>() {
+            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.hExists(keyByte, fieldByte);
+            }
+        }, true, false);
+        return b == null ? false : b;
+    }
+
+    /**
+     * hash结构中添加和修改value值
+     *
+     * @param key
+     * @param field
+     * @param value
+     * @return
+     */
+    public Boolean setHashValue(String key, String field, String value) {
+        final byte[] keyByte = key.getBytes();
+        final byte[] fieldByte = field.getBytes();
+        final byte[] valueByte = value.getBytes();
+        Boolean b = redisTemplate.execute(new RedisCallback<Boolean>() {
+            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.hSet(keyByte, fieldByte, valueByte);
+            }
+        }, true, false);
+        return b == null ? false : b;
+    }
 }
